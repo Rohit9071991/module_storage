ef9b1b969c3a6c44b22f3e8b0875c12b59c5843c
ROHIT <rohit.ksinha2012@gmail.com>
2025-01-03 21:48:39 +0530

    create new vnet

--- enviroment/dev/main.tf
+++ enviroment/dev/main.tf
@@ -2,10 +2,15 @@ module "resource_group" {
   source = "../../modules/rg"
   rgs    = var.rg_details
 }
-module "stotage_account" {
+module "storage_account" {
   depends_on = [module.resource_group]
   source     = "../../modules/storage_account"
   stg        = var.stg_details
 }
+module "vnet" {
+  depends_on = [ module.storage_account ]
+  source = "../../modules/virtual_network"
+  vnet = var.vnet_details
+  }
 
 
